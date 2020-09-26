7f9f8b0ef8995468bc28dde8f7ec53834d650a16
Chris Hackmann <cphackm@gmail.com>
2020-09-25 22:16:02 -0700

    Added base solution/project and test entity to prove WarnerEngine is working

---
+++ Warlocked/Scenes/PlayField.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using WarnerEngine.Lib;
+using WarnerEngine.Services;
+using WarnerEngine.Services.Implementations;
+
+namespace Warlocked.Scenes
+{
+    public class PlayField : Scene
+    {
+        public const string SceneKey = "play_field";
+
+        public override void OnSceneStart() 
+        {
+            Camera = new Camera(Vector2.Zero);
+            AddEntity(Camera);
+            AddEntity(new Entities.__Debug__.EternalZog());
+        }
+
+        public override void OnSceneEnd() 
+        {
+            entities.Clear();
+        }
+
+        public override void Draw()
+        {
+            GameService.GetService<IRenderService>()
+                .SetRenderTarget(SceneService.RenderTargets.CompositeTertiary, Color.Black)
+                .Start(Camera.GetCenterPoint())
+                .Render(base.Draw)
+                .End()
+                .Cleanup();
+        }
+
+        public override Dictionary<string, Func<string[], string>> GetLocalTerminalCommands()
+        {
+            return new Dictionary<string, Func<string[], string>>();
+        }
+    }
+}
