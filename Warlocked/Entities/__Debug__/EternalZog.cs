8d617129feaf0489ebde212023caf8250de70bcb
cphackm <cphackm@gmail.com>
2020-09-27 22:02:47 -0700

    Added base solution/project and test entity to prove WarnerEngine is working (#7)

---
+++ Warlocked/Entities/__Debug__/EternalZog.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using WarnerEngine.Lib;
+using WarnerEngine.Lib.Components;
+using WarnerEngine.Lib.Entities;
+using WarnerEngine.Services;
+
+namespace Warlocked.Entities.__Debug__
+{
+    public sealed class EternalZog : ISceneEntity, IPreDraw, IDraw
+    {
+        private float rotation;
+
+        public BackingBox GetBackingBox()
+        {
+            return BackingBox.Dummy;
+        }
+
+        public bool IsVisible()
+        {
+            return true;
+        }
+
+        public void OnAdd(Scene ParentScene) { }
+
+        public void OnRemove(Scene ParentScene) { }
+
+        public void PreDraw(float DT)
+        {
+            rotation += 2f * DT;
+        }
+
+        public void Draw()
+        {
+            Texture2D zogTexture = GameService.GetService<IContentService>().GetAsset<Texture2D>("zog_portrait");
+            GameService.GetService<IRenderService>().DrawQuad(
+                zogTexture, 
+                Vector2.Zero, 
+                new Rectangle(0, 0, zogTexture.Width, zogTexture.Height), 
+                Origin: new Vector2(zogTexture.Width / 2, zogTexture.Height / 2),
+                Rotation: rotation
+            );
+        }
+    }
+}
---
+++ Warlocked/WarlockedGame.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using WarnerEngine.Services;
+using WarnerEngine.Services.Implementations;
+
+namespace Warlocked
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class WarlockedGame : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public WarlockedGame()
+        {
+            GameService.Initialize();
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            GameService.GetService<IRenderService>()
+                .SetGraphicsDevice(GraphicsDevice)
+                .SetInternalResolution(640, 360);
+
+            GameService.GetService<IContentService>()
+                .Bootstrap(Content, GraphicsDevice)
+                .LoadAllContent();
+
+            GameService.GetService<ISceneService>()
+                .RegisterScene(Scenes.PlayField.SceneKey, new Scenes.PlayField())
+                .SetScene(Scenes.PlayField.SceneKey);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float DT = Math.Min((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f, 0.033f);
+
+            GameService.GetService<IStateService>().SetGlobalGameTime((float)gameTime.TotalGameTime.TotalMilliseconds);
+            GameService.GetService<IStateService>().IncrementGlobalFrameCount();
+
+            GameService.PreDraw(DT);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GameService.Draw();
+            GameService.PostDraw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
