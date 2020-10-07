4d555221f11070ba7844cff0f3bb63418f5127dc
Matt Hackmann <matt.hackmann@gmail.com>
2020-10-06 22:03:45 -0700

    basic scenario format and asset loader added

---
+++ Warlocked/Models/Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlocked.Models
+{
+    public class Map
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ Warlocked/Models/Objective.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlocked.Models
+{
+    public class Objective
+    {
+        public string Title { get; set; }
+        public Condition Condition { get; set; }
+    }
+}
